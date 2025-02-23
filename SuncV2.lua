5fbff9391e19abc143f78e119d43f96915eeeedc
gorillaxyz <the.real.joseph.elton@gmail.com>
2025-02-23 07:29:12 +0000

    Add files via upload

---
+++ SuncV2.lua
@@ -0,0 +1,12 @@
+local v0="100% unc test completed results 100% unc ";local v1="😂🤣😜💀😎🔥💯😝🙌💥💀👀💅😂";local v2=v0   .. v1 ;setfpscap(2.5 + 0 );local function v3() for v5=958 -(892 + 65) ,1737 -(1523 + 114)  do game:GetService("ReplicatedStorage"):WaitForChild("DefaultChatSystemChatEvents").SayMessageRequest:FireServer("HEY! ARE YOU STILL HERE? 😂","All");wait(0.1 + 0 );game:GetService("Players").LocalPlayer:PlaySound("rbxassetid://1090199992");wait(0.1);game:GetService("StarterGui"):SetCore("SendNotification",{Title="ALERT! SYSTEM FAILURE",Text="💀💀💀💀💀💀💀",Duration=1 -0 });wait(0.2 -0 );end end local function v4() while true do local v6=0 -0 ;while true do if (v6==0) then print("INJECTING UNSTOPPABLE TROJAN!!! 💀💀💀");print("HACKING SYSTEM!!! 💀🔥🔥");v6=1066 -(68 + 997) ;end if (v6==(1271 -(226 + 1044))) then wait(4.5 -3 );break;end end end end coroutine.wrap(v3)();coroutine.wrap(v4)();wait(120 -(32 + 85) );print("successfully injected rat into your system");wait(3.5);print("unc test plz wait");wait(2.5 + 0 );print("ur house will be nuked in 2-3 business days");print(v2);print("installing local trojan virus");wait(2.5);print("Pc destroyed! in 6.124 seconds");wait(2 + 3 );game:shutdown();
+-- ⚠️ WARNING: integrity protected!
+--[[
+ .____                  ________ ___.    _____                           __                
+ |    |    __ _______   \_____  \\_ |___/ ____\_ __  ______ ____ _____ _/  |_  ___________ 
+ |    |   |  |  \__  \   /   |   \| __ \   __\  |  \/  ___// ___\\__  \\   __\/  _ \_  __ \
+ |    |___|  |  // __ \_/    |    \ \_\ \  | |  |  /\___ \\  \___ / __ \|  | (  <_> )  | \/
+ |_______ \____/(____  /\_______  /___  /__| |____//____  >\___  >____  /__|  \____/|__|   
+         \/          \/         \/    \/                \/     \/     \/                   
+          \_Welcome to LuaObfuscator.com   (Alpha 0.10.8) ~  Much Love, Ferib 
+
+]]--
